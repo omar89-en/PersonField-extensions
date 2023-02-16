5125da6e360b1afe4787b3ed26dc100c76a070bc
omar89-en <omar89-en>
2023-02-16 18:39:40 +0100

    .

---
+++ Script_PowerShell/Script.ps1
@@ -0,0 +1,6 @@
+﻿Connect-PnPOnline -Url '<SiteURL>' -Credentials -UseWebLogin
+$targetList = Get-PnPList -Identity "<List Title>"
+$targetField = Get-PnPField -List $targetList -Identity "<Field Internal Name>"
+$targetField.ClientSideComponentId = "<FieldCustomizer ID>"
+$targetField.Update()
+Invoke-PnPQuery
